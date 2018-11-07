74f82bc03999bb8f0df56853df3c918e7a29366c
Alex Kalinkin <alex@kalinkin.info>
2018-11-07 16:27:29 +0300

    Requirements script updated

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,17 @@
 #!/bin/bash 
 
-apt-get install wmctrl
+if [ "$EUID" == 0 ]; then
+  echo "Please don't use sudo to run this script. Run it as regular user"
+  exit 1
+fi
+
+# Basic dependencies
+sudo apt-get install -y git python3-pip tmux vim urlview wmctrl taskwarrior
+# Vundle for Vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+# Time tracking hook
+pip3 install taskwarrior-time-tracking-hook
+mkdir -p ~/.task/hooks
+ln -s `which taskwarrior_time_tracking_hook` ~/.task/hooks/on-modify.timetracking
