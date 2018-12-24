d539908f404774dbc7dddccbd067385ad70cf038
Alex Kalinkin <alex@kalinkin.info>
2018-12-24 19:31:45 +0300

    Add UI notifications for ZenMode status

--- .scripts/zenmode.sh
+++ .scripts/zenmode.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # This script toggle Zen mode
-ZENMODESTATUSFILE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.zenmodestatus"
-echo "$ZENMODESTATUSFILE";
+STATUSFILE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/.zenmodestatus"
 
-usage="$(basename "$0") [-h] [-m n] -- zen mode script
+usage="$(basename "$0") [-h] [-m n] -- toggle or enable/disable terminal ZenMode 
 
 where: 
-    -h  show this help text 
+    -h  show this help text
     -m  set mode (0 - disabled, 1 - enabled)"
 
 # Process args
@@ -59,36 +58,46 @@ enable_mode() {
   disable_arrow_kews
   disable_mouse
   toggle_full_screen
+  notify-send -u low -t 1500 -a DOTFILES_SCRIPT "PowerUser" "ZenMode <b>Enabled</b>
+\r
+\r<i>ArrowKeys:</i> disabled
+\r<i>Mouse:</i> disabled
+\r<i>Fullscreen:</i> enabled"
 }
 
 disable_mode() {
   enable_arrow_keys 
   enable_mouse
   toggle_full_screen
+  notify-send -u low -t 1500 -a DOTFILES_SCRIPT "PowerUser" "ZenMode <b>Disabled</b>
+\r
+\r<i>ArrowKeys:</i> enabled
+\r<i>Mouse:</i> enabled
+\r<i>Fullscreen:</i> disabled"
 }
 
-if [ "$MODE" = 0 ]; then
-  echo "ZenMode: Disabled"
-#  disable_mode
-elif [ "$MODE" = 1 ]; then
-  echo "ZenMode: Enabled"
-#  enable_mode
-elif [ "$MODE" = -1 ]; then
-  echo "ZenMode param was not passed. Try to get from saved state"
-  if [ ! -w "$ZENMODESTATUSFILE" ]; then
+if [ "$MODE" = -1 ]; then
+# TODO: Echo line below only in Verbose mode
+#  echo "ZenMode param was not passed. Try to get from saved state"
+  if [ ! -w "$STATUSFILE" ]; then
     MODE=1
-    echo "$MODE" >> $ZENMODESTATUSFILE
   else 
-    MODE=`cat $ZENMODESTATUSFILE`
+    MODE=`cat $STATUSFILE`
     # toggle mode
     if [ "$MODE" = 1 ]; then MODE=0; else MODE=1;fi
   fi
+elif [ "$MODE" = 0 ] || [ "$MODE" = 1 ]; then
+  # it's alright the value in acceptable range
+  # TODO: Echo line below only in Verbose mode
+  echo "Correct -m option value"
 else 
   echo "Incorrect -m option value"
   echo "$usage" >&2
   exit -1
 fi
 
+echo "$MODE" > $STATUSFILE
+
 if [ "$MODE" = 0 ]; then disable_mode; fi
 if [ "$MODE" = 1 ]; then enable_mode; fi
-echo "$MODE" > $ZENMODESTATUSFILE
+
