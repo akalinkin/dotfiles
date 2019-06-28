e0a901cafd94fe80e332fd0037153a5c766f7d78
Alex Kalinkin <alex@kalinkin.info>
2019-06-28 21:39:28 +0300

    Add media volume buttons

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -1,10 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int borderpx  		= 1;        /* border pixel of windows */
+static const unsigned int snap      		= 32;       /* snap pixel */
+static const int showbar            		= 1;        /* 0 means no bar */
+static const int topbar             		= 1;        /* 0 means bottom bar */
+static const unsigned int systraypinning 	= 0;	/* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing 	= 2;	/* systray spacing */
+static const int systraypinningfailfirst 	= 1;	/* 1: if pinning fails, display systray on the first monitor, 0: display systray on the last monitor*/
+static const int showsystray 				= 1;		/* 0 means no systray */
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -90,36 +95,45 @@ static const char *toggleVolume[]      = { "amixer", "set", "Master", "toggle", 
 // // static const char *toggleVolume[]  = { "amixer", "-D", "pulse", "sset", "Master", "toggle", NULL }; // for ubuntu
 //
 
+/* mbp-mappings */
+#define XF86AudioMute			0x1008ff12
+#define XF86AudioLowerVolume	0x1008ff11
+#define XF86AudioRaiseVolume	0x1008ff13
+#define XF86TouchpadToggle		0x1008ffa9
+
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_w,      spawn,          {.v = chromiumcmd } },
-	{ MODKEY,                       XK_e,      spawn,          {.v = nautiluscmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_F12,	   spawn,	       {.v = upVolume5pre } },
-    { MODKEY,                   	XK_F11,	   spawn,	       {.v = downVolume5pre } },
-    { MODKEY,                   	XK_F10,	   spawn,	       {.v = toggleVolume } },
+	/* modifier                     key        				function        argument */
+	{ MODKEY,                       XK_d,      				spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, 				spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_w,      				spawn,          {.v = chromiumcmd } },
+	{ MODKEY,                       XK_e,      				spawn,          {.v = nautiluscmd } },
+	{ MODKEY,                       XK_b,      				togglebar,      {0} },
+	{ MODKEY,                       XK_j,      				focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      				focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      				incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_p,      				incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,      				setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,      				setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_Return, 				zoom,           {0} },
+	{ MODKEY,                       XK_Tab,    				view,           {0} },
+	{ MODKEY|ShiftMask,             XK_c,      				killclient,     {0} },
+	{ MODKEY,                       XK_t,      				setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      				setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      				setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_space,  				setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,  				togglefloating, {0} },
+	{ MODKEY,                       XK_0,      				view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      				tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  				focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, 				focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  				tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, 				tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_F12,	   				spawn,	       	{.v = upVolume5pre } },
+    { MODKEY,                   	XK_F11,	   				spawn,	       	{.v = downVolume5pre } },
+    { MODKEY,                   	XK_F10,	   				spawn,	       	{.v = toggleVolume } },
+	{ 0,                       		XF86AudioRaiseVolume,	spawn,	       	{.v = upVolume5pre } },
+	{ 0,                       		XF86AudioLowerVolume,	spawn,	       	{.v = downVolume5pre } },
+	{ 0,                       		XF86AudioMute,			spawn,	       	{.v = toggleVolume } },
     TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -129,7 +143,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      				quit,           {0} },
 };
 
 /* button definitions */
